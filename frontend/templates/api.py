0f5f294a38f182311136a97f6fa93abfb0a2142c
Iago Rivadulla <iagorivadullaact@gmail.com>
2026-01-26 19:19:20 +0100

    Change DATA_FILE to load dataset from URL
    Updated DATA_FILE to load dataset from Hugging Face.

--- frontend/templates/api.py
+++ frontend/templates/api.py
@@ -226,7 +226,9 @@ IAGO_DIR = REPO_ROOT / "Trabajo_Iago"
 
 # Dataset en TU backend (según tu estructura)
 DATA_DIR = BASE_DIR / "assets" / "data"
-DATA_FILE = DATA_DIR / "home_credit_train_ready.csv"
+DATA_FILE = DATA_FILE = load_dataset("csv",
+    data_files="https://huggingface.co/datasets/jamirc/home_credit_default_risk/resolve/main/home_credit_train_ready.csv",
+    split="train").to_pandas()
 
 # Modelo en repo root / models
 MODELS_DIR = REPO_ROOT / "models"
